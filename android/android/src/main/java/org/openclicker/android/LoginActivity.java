76e7b9af659ee6a8a9257256df4e4df719b11daf
Vincent <xue.vin@gmail.com>
2011-12-07 10:26:45 -0500

    Internet Permissions + Some quick logging

--- android/android/src/main/java/org/openclicker/android/LoginActivity.java
+++ android/android/src/main/java/org/openclicker/android/LoginActivity.java
@@ -17,10 +17,9 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import de.akquinet.android.androlog.Log;
-
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -30,63 +29,77 @@ public class LoginActivity extends Activity {
   private String studentUID;
   private String classUID;
   private String quizUID;
-  private String requestURL = "genome.hunter.cuny.edu:9998/quiz/";
+  // private String requestURL = "genome.hunter.cuny.edu:9998/quiz/";
   private String jString;
+  private static final String requestURL = "http://192.168.11.43:9998/quiz/";
+  private static final String TAG = "MyActivity";
+  
   private Vector<Integer> cUIDVec = new Vector<Integer>();
   private Vector<String> cDescriptionVec = new Vector<String>();
-
+  
   /** Called when the activity is first created. */
   @Override
   public void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     
-    // Initializes the logging
-    Log.init();
-    
     // Log a message (only on dev platform)
-    Log.i(this, "onCreate");
+    Log.i(TAG, "Random Logger Message");
     
     setContentView(R.layout.main);
-
-    final EditText studentText = (EditText) findViewById(R.id.editText1);
-    final EditText classText = (EditText) findViewById(R.id.editText2);
-    final EditText quizText = (EditText) findViewById(R.id.editText3);
-
-    final Button submitButton = (Button) findViewById(R.id.submitButton1);
+    
+    Button submitButton = (Button) findViewById(R.id.submitButton1);
     submitButton.setOnClickListener(new View.OnClickListener() {
       // When student press on the submit button, the program get the
       // Information from the 3 editText field and send request to the
       // server to get the quiz (with the helper function)
       public void onClick(View v) {
-        studentUID = studentText.getText().toString();
-        classUID = classText.getText().toString();
-        quizUID = quizText.getText().toString();
+        EditText studentText = (EditText) findViewById(R.id.editText1);
+        EditText classText = (EditText) findViewById(R.id.editText2);
+        EditText quizText = (EditText) findViewById(R.id.editText3);
         
-        // Create the request URL
-        requestURL = requestURL + quizUID + "/choices";
         try {
-          // Get the JSON from the server
-          jString = readData(requestURL);
+          
+          studentUID = studentText.getText().toString();
+          classUID = classText.getText().toString();
+          quizUID = quizText.getText().toString();
+          
+          if (studentUID.length() == 0 || classUID.length() == 0
+              || quizUID.length() == 0) {
+            Toast.makeText(LoginActivity.this, "Fill in the information",
+                Toast.LENGTH_SHORT).show();
+            return;
+          }
+          
+          // Create the request URL
+          String currentRequestURL = requestURL + quizUID + "/choices";
+          try {
+            // Get the JSON from the server
+            jString = readData(currentRequestURL);
+          } catch (Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+          }
+          
+          Toast.makeText(LoginActivity.this, jString, Toast.LENGTH_LONG).show();
+          
+          // Parse the JSON String, get the data and put them into the
+          // corresponding vector, pass them along to the next activity
+          try {
+            jParse(jString);
+          } catch (JSONException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+          }
+          
         } catch (Exception e) {
-          // TODO Auto-generated catch block
-          e.printStackTrace();
-        }
-        
-        Toast.makeText(LoginActivity.this, jString,
-            Toast.LENGTH_LONG).show();
-        
-        // Parse the JSON String, get the data and put them into the 
-        // corresponding vector, pass them along to the next activity
-        try {
-          jParse(jString);
-        } catch (JSONException e) {
-          // TODO Auto-generated catch block
-          e.printStackTrace();
+          Toast
+              .makeText(LoginActivity.this, e.getMessage(), Toast.LENGTH_SHORT)
+              .show();
         }
       }
     });
   }
-
+  
   // Connect to the server and read the JSON into a long string
   private String readData(String myRequestURL) throws Exception {
     BufferedReader in = null;
@@ -115,7 +128,7 @@ public class LoginActivity extends Activity {
       }
     }
   }
-
+  
   // JSON String parser method
   private void jParse(String myJString) throws JSONException {
     JSONObject root = new JSONObject(myJString);
